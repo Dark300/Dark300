8ea86a5cc80093fc5cbf9bc0e55f5d441b085a6d
pedrolo007 <64995496+Dark300@users.noreply.github.com>
2024-04-21 20:57:19 -0300

    Add files via upload
    Programação Web

---
+++ PW/PHP Introdução/index.php
@@ -0,0 +1,12 @@
+<?php
+$nome      = "Little";  //string
+$idade     = 16;        //inteiro
+$casado    = false;     //booleano
+$vazia     = null;      //nula
+$sobrenome = "Hair";    //string
+$salario   = 1500.00;   //double
+
+
+echo "Ola " .$nome." ".$sobrenome." Voce tem " .$idade ." anos de idade <br><br>";
+echo 'Ola $nome $sobrenome Voce tem $idade anos de idade'. '<br><br>';
+echo "Ola $nome $sobrenome Voce tem $idade anos de idade";
---
+++ PW/PHP Introdução/resultado.php
@@ -0,0 +1,5 @@
+<?php
+$nome    = $_GET['nome'];
+$email   = $_GET['email'];
+
+echo "Nome ".$nome." - Email ".$email;
